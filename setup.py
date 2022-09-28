79fdde6a5b6e01bea343cbf0d5fbb4e772af9cd0
tuoyl <tuoyl@ihep.ac.cn>
2022-09-28 14:07:10 +0800

    update Pypi API TOKEN name to 'PYPI_GITHUB_TOKEN'

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/tuoyl/gbm-pulsar'
 EMAIL = 'tuoyl@ihep.ac.cn'
 AUTHOR = 'Youli Tuo'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
